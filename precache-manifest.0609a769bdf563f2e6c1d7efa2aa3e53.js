ec31d54c35f696cffc9b4f954fe0a162578808a9
brendan hales <brendan_hales@hotmail.com>
2020-02-21 18:01:49 +0800

    Updates

---
+++ precache-manifest.0609a769bdf563f2e6c1d7efa2aa3e53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d766f63dd48f2dc01c9a578b342cffdd",
+    "url": "/RobotFriends/index.html"
+  },
+  {
+    "revision": "ac58112308d9ef2078bd",
+    "url": "/RobotFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1465a2b2d74cbe1322a7",
+    "url": "/RobotFriends/static/css/main.835476f6.chunk.css"
+  },
+  {
+    "revision": "ac58112308d9ef2078bd",
+    "url": "/RobotFriends/static/js/2.dc359369.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RobotFriends/static/js/2.dc359369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1465a2b2d74cbe1322a7",
+    "url": "/RobotFriends/static/js/main.b34f33d6.chunk.js"
+  },
+  {
+    "revision": "85b9cb15ad18d58f2804",
+    "url": "/RobotFriends/static/js/runtime-main.42426334.js"
+  }
+]);
